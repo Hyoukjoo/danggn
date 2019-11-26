0fffba9d1ff76f7210690fd0af6414a70dae620d
jo920208 <jo920208@gmail.com>
2019-11-26 21:53:14 +0900

    Add ecosystem.config.js

---
+++ server/ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: "danggn_server",
+      script: "build/app.js",
+      env_production: {
+        "PORT": 5000,
+        "NODE_ENV": "production"
+      }
+    }
+  ]
+}
